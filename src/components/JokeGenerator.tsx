0ecce107dbd9afdf9c66b2b50b0f29fd632fbb68
khuloodmaqbool <khulood.maqbool3@gmail.com>
2024-10-11 11:51:48 -0700

    resolve errors

--- src/components/JokeGenerator.tsx
+++ src/components/JokeGenerator.tsx
@@ -1,20 +1,17 @@
 "use client";
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 
-
 export default function JokeGenerator() {
-
-  const [apiData, setData] = useState<any>(null);
+  const [apiData, setData] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handlejokeGenerator = async () => {
+  const handleJokeGenerator = async () => {
     setLoading(true);
     setError(null);
     try {
-      const api = `https://icanhazdadjoke.com/`
+      const api = `https://icanhazdadjoke.com/`;
       const res = await fetch(api, {
         method: "GET",
         headers: {
@@ -25,29 +22,31 @@ export default function JokeGenerator() {
       if (!res.ok) throw new Error("Not found");
       const data = await res.json();
       setData(data.joke);
-
-    } catch (error: any) {
-      setLoading(false)
-      setError(error.message);
+    } catch (error: unknown) {
+      setError((error as Error).message);
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    handlejokeGenerator()
-  }, [])
+    handleJokeGenerator();
+  }, []);
 
   return (
-    <>
+    <div className="max-w-96 shadow rounded-lg mx-auto mt-8 p-5 bg-white">
+      <h1 className="font-bold mb-1 text-xl">&#128514; Random Joke Generator &#128514;</h1>
 
-      <div className="max-w-96 shadow rounded-lg mx-auto mt-8 p-5 bg-white">
-        <h1 className="font-bold mb-1 text-xl">&#128514; Random Joke Generator &#128514;</h1>
-
-        {loading ? <p>....loading</p> : error ? <p>error</p> : <p className="my-4" >{apiData}</p>}
-        <Button variant="outline" className="bg-green_col text-white" onClick={handlejokeGenerator} >{loading ? "...loading" : "Get New Joke"} &#128514;</Button>
-      </div>
-
-    </>
+      {loading ? (
+        <p>....loading</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <p className="my-4">{apiData}</p>
+      )}
+      <Button variant="outline" className="bg-green_col text-white" onClick={handleJokeGenerator}>
+        {loading ? "...loading" : "Get New Joke"} &#128514;
+      </Button>
+    </div>
   );
 }
